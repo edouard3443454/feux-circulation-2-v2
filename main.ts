d27836ea048e7f1b5ca4b16872ffd69f9ce69cf9
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-12-20 23:05:19 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,41 @@
-let a = 0
 input.onButtonPressed(Button.A, function () {
     a = 1
 })
-function lum () {
-	
-}
-function pieton () {
-	
-}
+let a = 0
+a = 0
 basic.forever(function () {
     if (a == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
-        pieton()
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        for (let index = 0; index < 4; index++) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+        }
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
         a = 0
     } else {
-        lum()
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(2000)
     }
 })
