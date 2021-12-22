a400f4b09d0020875f9523067a91c8d5d297dcc4
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-12-22 00:05:55 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     a = 1
 })
+function lum () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(2000)
+}
 let a = 0
 a = 0
 basic.forever(function () {
     if (a == 1) {
         if (pins.analogReadPin(AnalogPin.P0) == 1) {
+            pins.digitalWritePin(DigitalPin.P2, 0)
             pins.digitalWritePin(DigitalPin.P0, 1)
             pins.digitalWritePin(DigitalPin.P8, 1)
             basic.pause(5000)
@@ -28,16 +41,62 @@ basic.forever(function () {
             pins.digitalWritePin(DigitalPin.P2, 0)
             a = 0
         }
+        if (pins.analogReadPin(AnalogPin.P1) == 1) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            for (let index = 0; index < 4; index++) {
+                pins.digitalWritePin(DigitalPin.P8, 1)
+                basic.pause(1000)
+                pins.digitalWritePin(DigitalPin.P8, 0)
+            }
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            a = 0
+        }
+        if (pins.analogReadPin(AnalogPin.P2) == 1) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            for (let index = 0; index < 4; index++) {
+                pins.digitalWritePin(DigitalPin.P8, 1)
+                basic.pause(1000)
+                pins.digitalWritePin(DigitalPin.P8, 0)
+            }
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            a = 0
+        }
     } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(2000)
+        lum()
     }
 })
